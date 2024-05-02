7643b85378f331b43857e33c50a796a29bcc48ca
PatrizioCorcione <patriziocorcione1995@gmail.com>
2024-05-02 15:17:58 +0200

    [end]

--- index.php
+++ index.php
@@ -1,14 +1,36 @@
 <?php
+require_once __DIR__ . '/db.php';
+?>
 
-require_once __DIR__ . '/Model/Movie.php';
+<!DOCTYPE html>
+<html lang="en">
 
-$collateralBeauty = new Movie('Collaterl Beauty', 2012, '"Collateral Beauty" è un film drammatico che racconta la storia di Howard, un dirigente pubblicitario che affronta una profonda crisi dopo la morte improvvisa della figlia.', ['Drammatic', 'Sentimental', 'Romantic', 'Tragic']);
+<head>
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.css' integrity='sha512-VcyUgkobcyhqQl74HS1TcTMnLEfdfX6BbjhH8ZBjFU9YTwHwtoRtWSGzhpDVEJqtMlvLM2z3JIixUOu63PNCYQ==' crossorigin='anonymous' />
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Movie</title>
+</head>
 
-$theShawshankRedemption = new Movie('The Shawshank Redemption', 1994, 'Andy Dufresne, un banchiere condannato per omicidio, trascorre decenni nella prigione di Shawshank mentre cerca di dimostrare la sua innocenza e mantiene la speranza attraverso un amicizia improbabile con un altro detenuto.', ['Drammatic', 'Carcerario', 'Intrigante', 'Inspirazionale']);
+<body>
+  <main>
+    <div class="container my-5 ">
+      <div class="row row-cols-2 ">
 
-$inception = new Movie('Inception', 2010, 'In un mondo in cui è possibile entrare nei sogni degli altri, un esperto di furto di informazioni è incaricato di impiantare una nuova idea nella mente di un CEO rivale.', ['Azione', 'Fantascienza', 'Thriller', 'Complesso']);
+        <?php foreach ($movies as $movie) : ?>
+          <div class="col ">
+            <div class="card p-2 m-3">
+              <div class="card-body">
+                <h5 class="card-title"><?php echo $movie->title; ?></h5>
+                <h6 class="card-subtitle mb-2 text-body-secondary"><?php echo $movie->year; ?></h6>
+                <p class="card-text"><?php echo $movie->plot; ?></p>
+              </div>
+            </div>
+          </div>
+        <?php endforeach ?>
+      </div>
+    </div>
+  </main>
+</body>
 
-$forrestGump = new Movie('Forrest Gump', 1994, 'La vita straordinaria di Forrest Gump, un uomo con un QI basso ma un cuore d oro, che si trova coinvolto in molti degli eventi più significativi della storia americana.', ['Commedia', 'Drammatic', 'Romantic', 'Inspirazionale']);
-
-
-var_dump($collateralBeauty);
+</html>
