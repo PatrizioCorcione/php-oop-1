d30e9ff4568f73843201eacc765d42fba8a85c0a
PatrizioCorcione <patriziocorcione1995@gmail.com>
2024-05-02 14:37:52 +0200

    [fix] folder position

---
+++ Model/Movie.php
@@ -0,0 +1,24 @@
+<?php
+
+class Movie
+{
+  public $title;
+  public $year;
+  public $plot;
+  public $genres;
+  public $fullInfo;
+
+  public function __construct(string $_title, int $_year, string $_plot, array $_genres)
+  {
+    $this->title = $_title;
+    $this->year = $_year;
+    $this->plot = $_plot;
+    $this->genres = $_genres;
+    $this->fullInfo = $this->getFullInfo();
+  }
+
+  public function getFullInfo()
+  {
+    return $this->title . ' ' . $this->year;
+  }
+}
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once __DIR__ . '/Model/Movie.php';
+
+$collateralBeauty = new Movie('Collaterl Beauty', 2012, 'ciiciicoiicoicooicoocioicoicopiicoicoicooicoiociocioicoiocioicioiicoiiocioici', ['Drammatic', 'Sentimental', 'Romantic', 'Tragic']);
+
+var_dump($collateralBeauty);
